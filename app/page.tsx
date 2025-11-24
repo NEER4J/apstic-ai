97115b3320d858cf88cd3c93a35be9dc63e6e4ea
NEER4J <ittsneeraj@gmail.com>
2025-11-24 16:15:24 +0530

    m

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
               className="font-bold hover:underline"
               rel="noreferrer"
             >
-              Apstic
+              Neeraj
             </a>
           </p>
           <ThemeSwitcher />
--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center w-full">
 
-      <h1>Apstic</h1>
+      <h1>Yoo!</h1>
       <BeforeAfter />
     </div>
   );
